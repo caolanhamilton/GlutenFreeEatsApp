645e7948d28fcf22570d9d8f6211eee4178d7c09
caolanhamilton <caolanhamilton75@gmail.com>
2022-07-13 13:48:34 +0100

    Initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className="">This is the home page</Text>
+    </View>
+  )
+}
