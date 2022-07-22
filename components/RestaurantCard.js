096c149d78df4f72a53dbf3743092a87c15cfff6
caolanhamilton <caolanhamilton75@gmail.com>
2022-07-22 18:23:49 +0100

    Update restraunt card with correct properties from backend data

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,23 +1,50 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StarIcon } from "react-native-heroicons/solid";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { GOOGLE_MAPS_KEY } from "@env";
+import { MaterialIcons, Entypo, Ionicons } from "@expo/vector-icons";
 
 export default function RestaurantCard({ restaurant }) {
   return (
     <TouchableOpacity className="pr-2">
-      <Image className="h-56 w-72 rounded" source={{ uri: restaurant.image }} />
+      <Image
+        className="h-56 w-72 rounded"
+        source={{
+          uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photo_reference=${restaurant.image}&key=${GOOGLE_MAPS_KEY}`,
+        }}
+      />
       <Text className="font-bold text-lg pt-2">{restaurant.name}</Text>
+
       <View className={"flex-row items-center space-x-1"}>
         <MaterialCommunityIcons name="star" size={24} color="#6b21a8" />
         <Text className="text-gray-500 text-base font-bold">
-          {restaurant.rating}
+          {restaurant.avgRating}
         </Text>
+        <MaterialCommunityIcons
+          name="map-marker-multiple"
+          size={22}
+          color="#6b21a8"
+        />
         <Text className="text-gray-500 text-base">
-          {restaurant.category}
-          {" ·"}
+          {restaurant.distance.toFixed(1)} mi
         </Text>
-        <Text className="text-gray-500 text-base">{restaurant.distance}</Text>
+        <MaterialIcons name="security" size={20} color="#6b21a8" />
+        <Text className="text-gray-500 text-base">
+          {restaurant.avgSafetyRating}
+        </Text>
+        <View className="flex-row">
+          {restaurant.dedicatedGlutenFree ? (
+            <>
+              <Ionicons
+                name="checkmark-circle-sharp"
+                size={22}
+                color="#6b21a8"
+              />
+              <Text className="text-gray-500 text-base">DGF</Text>
+            </>
+          ) : null}
+        </View>
       </View>
     </TouchableOpacity>
   );
