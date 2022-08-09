5e5562a3e1cbf79a46e84924d1e7c5310185fb39
caolanhamilton <caolanhamilton75@gmail.com>
2022-08-09 17:57:12 +0100

    Create file for firebase authentication functions

---
+++ firebaseAuthFuncs.js
@@ -0,0 +1,87 @@
+// import { firebaseConfig } from "../config";
+import firebase from "firebase/compat/app";
+import { Alert } from "react-native";
+
+export const getIdToken = () => {
+  return firebase
+    .auth()
+    .currentUser?.getIdToken(/* forceRefresh */ true)
+    .then(function (idToken) {
+      return idToken;
+    })
+    .catch(function (error) {
+      console.log("could not get id token", error);
+    });
+};
+
+export const login = (
+  email,
+  password,
+  setEmail,
+  setPassword,
+  setConfirmPassword,
+) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userInfo) => {
+      Alert.alert("Success", "You are logged in");
+      setEmail("");
+      setConfirmPassword("");
+      setPassword("");
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error.code === "auth/invalid-email") {
+        Alert.alert("Error", "Incorrect email formatting");
+      }
+      if (error.code === "auth/user-not-found") {
+        Alert.alert("Sorry", "User with this email not found");
+      } else if (error.code === "auth/wrong-password") {
+        Alert.alert("Sorry", "Wrong password");
+      }
+    });
+};
+
+export const register = (email, password) => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      Alert.alert("Success", "You are registered!");
+    })
+    .catch((error) => {
+      if (error.code === "auth/email-already-in-use") {
+        Alert.alert("Sorry", "Email already in use");
+      } else if (error.code === "auth/invalid-email") {
+        Alert.alert("Sorry", "Email not valid");
+      } else if (error.code === "auth/weak-password") {
+        Alert.alert("Sorry", "Password must be at least 6 characters");
+      }
+    });
+};
+
+export const resetPassword = (email) => {
+  firebase
+    .auth()
+    .sendPasswordResetEmail(email)
+    .then(() => {
+      Alert.alert("Success", "Password reset email sent");
+    })
+    .catch((error) => {
+      console.log(error);
+      if (
+        error.code === "auth/user-not-found" ||
+        error.code === "auth/invalid-email"
+      ) {
+        return Alert.alert("Sorry", "User with this email not found");
+      }
+      if (error.code === "auth/missing-email") {
+        Alert.alert("Sorry", "You haven't entered an email");
+      }
+    });
+};
+
+export const logout = () => {
+  firebase.auth().signOut();
+};
