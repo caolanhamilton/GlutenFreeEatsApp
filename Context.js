5cb512dd1c398ba088fe82bad69e11dec8de778b
caolanhamilton <caolanhamilton75@gmail.com>
2022-08-16 09:16:29 +0100

    Add userLocations to context

--- Context.js
+++ Context.js
@@ -5,12 +5,18 @@ export const AuthContext = createContext();
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [userFavouritedLocations, setUserFavouritedLocations] = useState([]);
+  const [userLocations, setUserLocations] = useState([]);
+  const [userPostedReviews, setUserPostedReviews] = useState([]);
   
   return (
     <AuthContext.Provider
       value={{
         user,
         setUser,
+        userPostedReviews,
+        setUserPostedReviews,
+        userLocations,
+        setUserLocations,
         userFavouritedLocations,
         setUserFavouritedLocations,
       }}
