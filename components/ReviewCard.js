72f9dfcc87344400b01986604c983739ef1cbf54
caolanhamilton <caolanhamilton75@gmail.com>
2022-08-19 14:45:19 +0100

    Display name or you beside review depending on who posted it

--- components/ReviewCard.js
+++ components/ReviewCard.js
@@ -1,11 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
-import {
-  MaterialCommunityIcons,
-  MaterialIcons,
-} from "@expo/vector-icons";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { useContext } from "react";
+import { AuthContext } from "../Context";
 
 export default function ReviewCard({ review }) {
+  const { user } = useContext(AuthContext);
   return (
     <View className="bg-gray-200 mx-4 my-1 p-2 rounded-lg">
       <View className="flex-row items-center">
@@ -15,7 +15,12 @@ export default function ReviewCard({ review }) {
         <Text>{review.safetyRating?.toFixed(1)}</Text>
       </View>
       <Text className="text-[16px]">{review.reviewText}</Text>
-      <Text className="text-right text-xs right-1">by username</Text>
+      <Text className="text-right text-xs right-1">
+        Posted by
+        {review.userId === user.uid
+          ? " you."
+          : review.userFirstName + " " + review.userLastName[0]}
+      </Text>
     </View>
   );
 }
