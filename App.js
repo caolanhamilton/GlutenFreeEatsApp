65262ffbeaaf660816d393fa7e73421a7989d673
caolanhamilton <caolanhamilton75@gmail.com>
2022-08-19 13:19:37 +0100

    Reformatting

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import HomeScreen from "./screens/HomeScreen";
 import AddRestaurant from "./screens/AddRestaurant";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import VerticalRestaurantsList from "./screens/VerticalRestaurantsListPage";
-import UserLocations from "./screens/UserLocations"; 
+import UserLocations from "./screens/UserLocations";
 import { AuthContextProvider } from "./Context";
 import LoginReg from "./screens/LoginReg";
 import UserReviews from "./screens/UserReviews";
@@ -17,38 +17,44 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <AuthContextProvider>
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <TailwindProvider>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
-            <Stack.Screen
-              name="AddRestaurant"
-              component={AddRestaurant}
-            ></Stack.Screen>
-            <Stack.Screen
-              name="RestaurantScreen"
-              component={RestaurantScreen}
-            ></Stack.Screen>
-            <Stack.Screen
-              name="VerticalRestaurants"
-              component={VerticalRestaurantsList}
-            ></Stack.Screen>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <TailwindProvider>
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+              <Stack.Screen
+                name="AddRestaurant"
+                component={AddRestaurant}
+              ></Stack.Screen>
+              <Stack.Screen
+                name="RestaurantScreen"
+                component={RestaurantScreen}
+              ></Stack.Screen>
+              <Stack.Screen
+                name="VerticalRestaurants"
+                component={VerticalRestaurantsList}
+              ></Stack.Screen>
               <Stack.Screen name="LoginReg" component={LoginReg}></Stack.Screen>
-              <Stack.Screen name="UserLocations" component={UserLocations}></Stack.Screen>
-              <Stack.Screen name="UserReviews" component={UserReviews}></Stack.Screen>
-          </Stack.Navigator>
-        </TailwindProvider>
-      </NavigationContainer>
+              <Stack.Screen
+                name="UserLocations"
+                component={UserLocations}
+              ></Stack.Screen>
+              <Stack.Screen
+                name="UserReviews"
+                component={UserReviews}
+              ></Stack.Screen>
+            </Stack.Navigator>
+          </TailwindProvider>
+        </NavigationContainer>
       </SafeAreaProvider>
-      </AuthContextProvider>
+    </AuthContextProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#white",
     alignItems: "center",
     justifyContent: "center",
   },
