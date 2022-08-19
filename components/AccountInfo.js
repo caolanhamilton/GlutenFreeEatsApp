652db47a0e000316203739501ea75110e746a4f7
caolanhamilton <caolanhamilton75@gmail.com>
2022-08-19 14:44:51 +0100

    Remove accidental boarder

--- components/AccountInfo.js
+++ components/AccountInfo.js
@@ -25,7 +25,7 @@ export default function AccountInfo() {
       <View className="self-between gap-x-2 flex-row items-center mb-4">
         <TouchableOpacity
           onPress={() => {navigation.navigate("UserReviews")}}
-          className="bg-white border-2 border-white h-12 rounded-3xl p-2 w-1/3 mb-3"
+          className="bg-white  h-12 rounded-3xl p-2 w-1/3 mb-3"
         >
           <Text className="text-center color-purple-800 font-semibold text-lg">
             Reviews
@@ -33,7 +33,7 @@ export default function AccountInfo() {
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => {navigation.navigate("UserLocations")}}
-          className="bg-white border-2 border-white h-12 rounded-3xl p-2 w-30 mb-3 w-1/3"
+          className="bg-white  h-12 rounded-3xl p-2 w-30 mb-3 w-1/3"
         >
           <Text className="text-center color-purple-800 font-semibold text-lg">
             Locations
