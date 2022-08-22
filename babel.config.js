640d4d2761275ec214cd37ea89e9e45c0a6a01d6
caolanhamilton <caolanhamilton75@gmail.com>
2022-08-22 19:00:26 +0100

    Update babel config with react-reanimated

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["tailwindcss-react-native/babel"], ["module:react-native-dotenv", {
+    plugins: [["tailwindcss-react-native/babel"], ['react-native-reanimated/plugin'], ["module:react-native-dotenv", {
         "moduleName": "@env",
         "path": ".env",
         "blacklist": null,
